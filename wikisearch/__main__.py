1f63e266c38cac18f70046043fdf871014e3121a
George Perdrizet <george@perdrizet.org>
2024-04-04 11:51:58 -0400

    Cleaned up a bit.

--- wikisearch/__main__.py
+++ wikisearch/__main__.py
@@ -3,18 +3,21 @@ from wikisearch import parse_dump
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(
+    # Set-up command line argument parser
+    parser=argparse.ArgumentParser(
         prog='wikisearch.py',
         description='Run wikisearch tasks',
         formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=80)
     )
 
+    # Add argument for task to run
     parser.add_argument(
         'task',
         choices=['update_dump', 'parse_dump'],
         help='Task to run'
     )
 
+    # Add argument for parsed output destination
     parser.add_argument(
         '--output',
         required=False,
@@ -23,8 +26,10 @@ if __name__ == '__main__':
         help='Where to output parsed articles'
     )
 
-    args = parser.parse_args()
+    args=parser.parse_args()
 
+    # Decide what to do and how to do it based on
+    # user provided arguments
     if args.task == 'parse_dump':
         parse_dump.run(args.output)
 
