650bb9d7236bc2afd347e8ff7c4421129c49ea33
George Perdrizet <george@perdrizet.org>
2024-04-04 11:35:52 -0400

    Added command line args to __main__.py to select task to run and what to do with parsed output.

--- wikisearch/__main__.py
+++ wikisearch/__main__.py
@@ -1,5 +1,35 @@
+import argparse
 from wikisearch import parse_dump
 
 if __name__ == '__main__':
 
-    parse_dump.run()
+    parser = argparse.ArgumentParser(
+        prog='wikisearch.py',
+        description='Run wikisearch tasks',
+        formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=80)
+    )
+
+    parser.add_argument(
+        '--task',
+        required=True,
+        choices=['update_dump', 'parse_dump'],
+        help='Task to run'
+    )
+
+    parser.add_argument(
+        '--output',
+        required=False,
+        choices=['file', 'opensearch'],
+        help='Where to output parsed articles'
+    )
+
+    args = parser.parse_args()
+
+    if args.task == 'parse_dump':
+        parse_dump.run(args.output)
+
+    elif args.task == 'update_dump':
+        pass
+
+    else:
+        print('Unrecognized task, exiting.')
