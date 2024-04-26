9cf08afdb000fe8ac134bc3bb8902f41fae21f6a
George Perdrizet <george@perdrizet.org>
2024-04-26 14:32:14 -0400

    Started work on utility to generate a small sample of data from a dump file for rapid prototyping/testing.

--- wikisearch/make_sample.py
+++ wikisearch/make_sample.py
@@ -1,6 +1,9 @@
 '''Function to take a small sample of records in a dump file
 and save as a separate file. Used to rapid prototyping/testing.'''
 
+from bz2 import BZ2File
+from gzip import GzipFile
+
 def run(dump: str) -> None:
 
     # Figure out what type of dump we are working with based
@@ -9,8 +12,22 @@ def run(dump: str) -> None:
     if '.'.join(dump.split('.')[-2:]) == 'xml.bz2':
         print(f'Sampling XML dump file')
 
+    # Handle CirrusSearch dump
     elif '.'.join(dump.split('.')[-2:]) == 'json.gz':
         print(f'Sampling CirrusSearch dump')
+
+        # Empty list to hold lines for output
+        output_list = []
+
+        # Open dump file as an input stream
+        input_stream=GzipFile(dump)
+
+        # Count lines, stopping when we have 10000 (or 5000 articles)
+        n=0
+
+        while n < 10000:
+            output_list.append(next(input_stream))
+            n+=1
 
     else:
         print('Unrecognized dump file type')
