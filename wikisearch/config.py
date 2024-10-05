054387b21f5f0b11f91b5df33661f65056f3d3d8
George Perdrizet <george@perdrizet.org>
2024-10-05 18:28:40 -0400

    Updated comments.

--- wikisearch/config.py
+++ wikisearch/config.py
@@ -13,25 +13,28 @@ MODEL_ID='oKFpWpIBgWKaVuCy3vi8'
 NLP_INGEST_PIPELINE_DESCRIPTION='An NLP ingest pipeline'
 NLP_INGEST_PIPELINE_ID='nlp_ingest_pipeline'
 
-# Number of workers to start for parsing documents
+# Default number of workers to start for parsing documents, can be overridden
+# via command line argument
 XML_PARSE_WORKERS=15
 CS_PARSE_WORKERS=1
 
-# Number of workers to start for outputting parsed
-# documents to file or OpenSearch index
+# Default number of workers to start for outputting parsed
+# documents to file or OpenSearch index can be overridden
+# via command line argument
 XML_OUTPUT_WORKERS=1
 CS_OUTPUT_WORKERS=4
 
-# Number of documents to index via bulk call to OpenSearch
+# Default number of documents to index via bulk call to OpenSearch
+# can be overridden via command line argument
 BULK_BATCH_SIZE=100
 
-# Dump data files
-XML_INPUT_FILE='wikisearch/data/enwiki-20240320-pages-articles-multistream.xml.bz2'
-CS_INPUT_FILE='wikisearch/data/enwiki-20240401-cirrussearch-content.json.gz'
+# Default dump data files can be overridden via command line argument
+XML_INPUT_FILE='wikisearch/data/enwiki-20240320-pages-articles-multistream.sample.xml.bz2'
+CS_INPUT_FILE='wikisearch/data/enwiki-20240401-cirrussearch-content.sample.json.gz'
 
-# OpenSearch index names
-XML_INDEX='enwiki-xml'
-CS_INDEX='enwiki-cs'
+# OpenSearch index names can be overridden via command line argument
+XML_INDEX='neural_xml_test'
+CS_INDEX='neural_cs_test'
 
 # Index to use for search test
-TEST_SEARCH_INDEX='enwiki-xml'
+TEST_SEARCH_INDEX='neural_cs_test'
