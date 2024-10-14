15b53da4915fb27d026a18a57bb5948a51840b38
George Perdrizet <george@perdrizet.org>
2024-10-13 22:43:56 -0400

    Updated notebook function calls.

--- semantic_search/functions/notebook_helper.py
+++ semantic_search/functions/notebook_helper.py
@@ -1,5 +1,14 @@
 '''Collection of functions for notebooks.'''
 
+# PyPI imports
+from opensearchpy import OpenSearch # pylint: disable = import-error
+
+# Internal imports
+import configuration as config
+
+############################################################
+# OWikipedia data cleaning functions #######################
+############################################################
 
 def remove_extra_sections(source_string: str) -> str:
     '''Remove extra sections from the end of the document by splitting 
@@ -127,3 +136,72 @@ def remove_thumbnails(source_string: str) -> str:
     source_string='\n'.join(cleaned_source_array)
 
     return source_string
+
+############################################################
+# OpenSearch functions #####################################
+############################################################
+
+def start_client() -> OpenSearch:
+
+    '''Fires up the OpenSearch client'''
+
+    # Set host and port
+    host='localhost'
+    port=9200
+
+    # Create the client with SSL/TLS and hostname verification disabled.
+    client=OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compress=False,
+        timeout=30,
+        use_ssl=False,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False
+    )
+
+    return client
+
+def initialize_index(index_name: str) -> None:
+
+    '''Set-up OpenSearch index. Deletes index if it already exists
+    at run start. Creates new index for run.'''
+
+    client=start_client()
+
+    # Delete the index we are trying to create if it exists
+    if client.indices.exists(index=index_name):
+        _=client.indices.delete(index=index_name)
+
+    # Create the target index if it does not exist
+    if client.indices.exists(index=index_name) is False:
+
+        index_body={
+            "settings": {
+                "number_of_shards": 3,
+                "index.knn": "true",
+                "default_pipeline": f'{config.INGEST_PIPELINE_ID}'
+            },
+            "mappings": {
+                "properties": {
+                    "text_embedding": {
+                        "type": "knn_vector",
+                        "dimension": 768,
+                        "method": {
+                        "engine": "lucene",
+                        "space_type": "l2",
+                        "name": "hnsw",
+                        "parameters": {}
+                        }
+                    },
+                    "text": {
+                        "type": "text"
+                    }
+                }
+            }
+        }
+
+        _=client.indices.create(index_name, body=index_body)
+
+    # Close client
+    client.close()
